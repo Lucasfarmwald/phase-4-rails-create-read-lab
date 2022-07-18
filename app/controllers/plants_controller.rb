66d04b52284ad57524c3f217604468d9277673c7
lucasfarmwald <farmwaldlucas@gmail.com>
2022-07-18 13:49:12 -0500

    commit tree d2e668f87a4b790acac45fd44587ea7893d05672

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,24 @@
+class PlantsController < ApplicationController
+
+    def index
+        render json: Plant.all
+    end
+
+    def show
+        plants = Plant.find(params[:id])
+
+        render json: plants
+    end
+
+    def create
+        plants = Plant.create(plant_params)
+        render json: plants, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
